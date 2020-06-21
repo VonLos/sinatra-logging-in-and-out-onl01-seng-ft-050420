8f4b9a7c1723cf309fbd30e968882c4630f288f2
rylandgrounds <rylandgrounds@gmail.com>
2020-06-21 12:15:20 -0500

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+    def self.current_user(sessions)
+        @user = User.find(sessions[:user_id])
+        @user
+        
+    end
+
+
+    def self.is_logged_in?(sessions)
+      @user = User.find(sessions[:user_id]) 
+      @user.id == sessions[:user_id]
+    
+    end
 end
